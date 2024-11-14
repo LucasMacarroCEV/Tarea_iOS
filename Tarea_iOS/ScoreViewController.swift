0e080df4c11362ed0a7b6bb816f9fa078eedd247
Lucas <lucas_macarro_m-tsapp1ma2324@cev.com>
2024-11-14 14:35:36 +0100

    iOS_Task - Version 0.92

--- Tarea_iOS/ScoreViewController.swift
+++ Tarea_iOS/ScoreViewController.swift
@@ -4,6 +4,7 @@ import UIKit
 class ScoreViewController: UIViewController, UITableViewDataSource {
     
     var users: [User] = []
+    var apikey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFoYXZydmtobGJtc2xqZ21ia25yIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDA3MjY5MTgsImV4cCI6MjAxNjMwMjkxOH0.Ta-_lXGGwSiUGh0VC8tAFcFQqsqAvB8vvXJjubeQkx8"
 
     @IBOutlet weak var changeUserBTN: UIButton!
     @IBOutlet weak var restartBTN: UIButton!
@@ -12,7 +13,7 @@ class ScoreViewController: UIViewController, UITableViewDataSource {
     override func viewDidLoad() {
         super.viewDidLoad()
         Start()
-        GetApiScores()
+        ManageApiData()
     }
     
     func Start() {
@@ -50,53 +51,96 @@ class ScoreViewController: UIViewController, UITableViewDataSource {
     }
     
     func ManageApiData() {
-        GetApiScores()
-        for x in users {
-            print("Nombre:", x.name, "\nPuntuacion max:", x.maxScore, "\n")
-        }
-        if CheckExistingUser(usersArray: users) {
-            let existingUserIndex = GetExistingUserIndex(usersArray: users)
-            if users[existingUserIndex].maxScore < currentUser!.maxScore {
-                print("El ususario existe en la base de datos")
-                //PatchApiScores()
-            }
-        }
-        else {
-            PostApiScores(name: currentUser!.name, score: currentUser!.maxScore)
-            print("El usuario ha sido añadido a la base de datos")
-        }
-        GetApiScores()
-        for x in users {
-            print("Nombre:", x.name, "\nPuntuacion max:", x.maxScore, "\n")
-        }
-        users = users.sorted{$0.maxScore > $1.maxScore}
-        tableView.reloadData()
-        Loaded()
-    }
-    func GetApiScores() {
-        users.removeAll()
-        if let url = URL(string: "https://qhavrvkhlbmsljgmbknr.supabase.co/rest/v1/scores?select=*") {
-            var request = URLRequest(url: url)
-            request.httpMethod = "GET"
-            request.setValue("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFoYXZydmtobGJtc2xqZ21ia25yIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDA3MjY5MTgsImV4cCI6MjAxNjMwMjkxOH0.Ta-_lXGGwSiUGh0VC8tAFcFQqsqAvB8vvXJjubeQkx8", forHTTPHeaderField: "apikey")
-            let task = URLSession.shared.dataTask(with: request) {(data, response, error) in
-                guard let data = data else {return}
+        
+        
+        
+        GetApiScores {[self] result in
+            switch result {
+            case.success(let data):
                 do {
                     let usersResponse = try JSONDecoder().decode([UserResponse].self, from: data)
                     for x in usersResponse {
-                        self.users.append(User(name: x.name, maxScore: x.score))
+                        users.append(User(name: x.name, maxScore: x.score))
                     }
-                    DispatchQueue.main.async {
-                        self.ManageApiData()
+                    if CheckExistingUser(usersArray: users) {
+                        let existingUserIndex = GetExistingUserIndex(usersArray: users)
+                        if users[existingUserIndex].maxScore < currentUser!.maxScore {
+                            print("El ususario existe en la base de datos")
+                            //PatchApiScores()
+                        }
+                    }
+                    else {
+                        //PostApiScores(name: currentUser!.name, score: currentUser!.maxScore)
+                        print("El usuario ha sido añadido a la base de datos")
                     }
                 }
                 catch let errorJson {
                     self.Error()
                     print(errorJson)
                 }
+            case .failure(let error):
+                print(error.localizedDescription)
+                self.Error()
+            }
+        }
+        GetApiScores {[self] result in
+            switch result {
+            case.success(let data):
+                do {
+                    let usersResponse = try JSONDecoder().decode([UserResponse].self, from: data)
+                    for x in usersResponse {
+                        users.append(User(name: x.name, maxScore: x.score))
+                    }
+                    DispatchQueue.main.async {
+                        self.users = self.users.sorted{$0.maxScore > $1.maxScore}
+                        self.tableView.reloadData()
+                        self.Loaded()
+                    }
+                }
+                catch let errorJson {
+                    self.Error()
+                    print(errorJson)
+                }
+            case .failure(let error):
+                print(error.localizedDescription)
+                self.Error()
+            }
+        }
+    }
+    func GetApiScores(completion: @escaping (Result<Data, Error>) -> Void) {
+        if let url = URL(string: "https://qhavrvkhlbmsljgmbknr.supabase.co/rest/v1/scores?select=*") {
+            var request = URLRequest(url: url)
+            request.httpMethod = "GET"
+            request.setValue(apikey, forHTTPHeaderField: "apikey")
+            
+            let task = URLSession.shared.dataTask(with: request) {(data, response, error) in
+                if let error = error {completion(.failure(error)); return}
+                guard let httpResponse = response, let data = data else {completion(.failure(error!)); return}
+                completion(.success(data))
             }.resume()
         }
-        
+//        users.removeAll()
+//        if let url = URL(string: "https://qhavrvkhlbmsljgmbknr.supabase.co/rest/v1/scores?select=*") {
+//            var request = URLRequest(url: url)
+//            request.httpMethod = "GET"
+//            request.setValue(apikey, forHTTPHeaderField: "apikey")
+//            let task = URLSession.shared.dataTask(with: request) {(data, response, error) in
+//                guard let data = data else {return}
+//                do {
+//                    let usersResponse = try JSONDecoder().decode([UserResponse].self, from: data)
+//                    for x in usersResponse {
+//                        self.users.append(User(name: x.name, maxScore: x.score))
+//                    }
+//                    DispatchQueue.main.async {
+//                        //self.ManageApiData()
+//                    }
+//                }
+//                catch let errorJson {
+//                    self.Error()
+//                    print(errorJson)
+//                }
+//            }.resume()
+//        }
     }
     func PostApiScores(name:String, score:Int) {
         let parameters: [String: Any] = ["name": name, "score": score]
@@ -105,7 +149,7 @@ class ScoreViewController: UIViewController, UITableViewDataSource {
         if let url = URL(string: "https://qhavrvkhlbmsljgmbknr.supabase.co/rest/v1/scores") {
             var request = URLRequest(url: url)
             request.httpMethod = "POST"
-            request.setValue("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFoYXZydmtobGJtc2xqZ21ia25yIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDA3MjY5MTgsImV4cCI6MjAxNjMwMjkxOH0.Ta-_lXGGwSiUGh0VC8tAFcFQqsqAvB8vvXJjubeQkx8", forHTTPHeaderField: "apikey")
+            request.setValue(apikey, forHTTPHeaderField: "apikey")
             request.setValue("application/json", forHTTPHeaderField: "Content-Type")
             request.httpBody = parametersJSON
             let task = URLSession.shared.dataTask(with: request) {(data, response, error) in
@@ -120,35 +164,6 @@ class ScoreViewController: UIViewController, UITableViewDataSource {
                 }
             }.resume()
         }
-
-        
-//        let parameters: [String : Any] = ["name" : name, "score" : score]
-//        
-//        if let url = URL(string: "https://qhavrvkhlbmsljgmbknr.supabase.co/rest/v1/scores") {
-//            var request = URLRequest(url: url)
-//            do {
-//                request.httpBody = try JSONSerialization.data(withJSONObject: parameters, options: .prettyPrinted)
-//            } catch let error {
-//                Error()
-//                print(error.localizedDescription)
-//            }
-//            request.httpMethod = "POST"
-//            request.setValue("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFoYXZydmtobGJtc2xqZ21ia25yIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDA3MjY5MTgsImV4cCI6MjAxNjMwMjkxOH0.Ta-_lXGGwSiUGh0VC8tAFcFQqsqAvB8vvXJjubeQkx8", forHTTPHeaderField: "apikey")
-//            let task = URLSession.shared.dataTask(with: request) {[self] (data, response, error) in
-//                guard error == nil else {Error(); return}
-//                guard let data = data else {return}
-//                do {
-//                    guard let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? [String: Any] else {
-//                        Error()
-//                        return
-//                    }
-//                    print(json)
-//                } catch let error {
-//                    Error()
-//                    print(error.localizedDescription)
-//                }
-//            }.resume()
-//        }
     }
     func PatchApiScores() {
         
